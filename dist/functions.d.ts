5d2c782c8d84d2c988800e7a7df600133fb07df1
MateusLiraFront <mlgtifront@gmail.com>
2026-09-15 19:35:40 -0300

    alteração diretorios

--- dist/functions.d.ts
+++
@@ -1,9 +0,0 @@
-import { Perfil } from "./profile";
-export declare function registrarPeso(perfil: Perfil, novoPeso: number): Perfil;
-export declare function registrarAltura(perfil: Perfil, altura: number): Perfil;
-export declare function registrarConsumoAgua(perfil: Perfil, agua: number): Perfil;
-export declare function registrarCarboidrato(perfil: Perfil, carb: number): Perfil;
-export declare function registrarProteina(perfil: Perfil, proteina: number): Perfil;
-export declare function registrarTreino(perfil: Perfil, treino: boolean): Perfil;
-export declare function registrarDistancia(perfil: Perfil, distancia: number): Perfil;
-//# sourceMappingURL=functions.d.ts.map
